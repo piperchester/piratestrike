166fecaa663000c6f55d46d4cf818a066f376ba7
piper <piperchester@gmail.com>
2012-12-16 14:38:29 -0500

    Initial commit of battleship.py

---
+++ battleship.py
@@ -0,0 +1,38 @@
+mport random
+
+board = []
+
+for x in range(0,5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return random.randint(0,len(board)-1)
+
+def random_col(board):
+    return random.randint(0,len(board[0])-1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+guess_row = input("Guess Row:")
+guess_col = input("Guess Col:")
+
+print ship_row
+print ship_col
+
+if guess_row ==  ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"
+else:
+    if (guess_row < 0 or guess_row > 4 or guess_col < 0 or guess_col > 4):
+        print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == x:
+        print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        print guess_row
+        print guess_col
